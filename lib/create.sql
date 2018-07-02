2fc7ef97c59a836c1b1e1d3597ad36b8088bb405
Ryan Davey <ryancdavey@icloud.com>
2018-07-02 05:47:43 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,10 +1,10 @@
 CREATE TABLE projects (
-id INTEGER PRIMARY KEY,
-title TEXT,
-category TEXT,
-funding_goal NUMERIC,
-start_date TEXT,
-end_date TEXT
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal NUMERIC,
+  start_date TEXT,
+  end_date TEXT
 )
 ;
 
